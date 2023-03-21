68ac139e81314ba76376d77e1c02a1f74e3160f8
bazileros <zalisilesithule@gmail.com>
2023-03-21 16:35:52 -0700

    0x02 Functions Nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,40 +1,35 @@
+#include <stdio.h>
 #include "main.h"
 
 
-	/**
-	 * times_table - prints the 9 times table, starting with 0
-	 */
-	void times_table(void)
-	{
-		int i, j, k;
+/**
+* print_to_98 - prints all natural numbers from n to 98,
+* followed by a new line
+* @n: print from this number
+*/
+void print_to_98(int n)
+{
+int i, j;
 
 
-		for (i = 0; i < 10; i++)
-		{
-			for (j = 0; j < 10; j++)
-			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				}
-
-
-				if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-			}
-			_putchar('\n');
-		}
-	}
-
+if (n <= 98)
+{
+for (i = n; i <= 98; i++)
+{
+if (i != 98)
+printf("%d, ", i);
+else if (i == 98)
+printf("%d\n", i);
+}
+}
+else if (n >= 98)
+{
+for (j = n; j >= 98; j--)
+{
+if (j != 98)
+printf("%d, ", j);
+else if (j == 98)
+printf("%d\n", j);
+}
+}
+}
