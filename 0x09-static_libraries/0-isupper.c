750a975f44c9771e90139c869684216348c6ffb9
bazileros <zalisilesithule@gmail.com>
2023-04-12 13:17:39 -0700

    0x09 static libraries

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -5,14 +5,10 @@
  * @c: char to check
  *
  * Return: 0 or 1
- */
-int main()
-{
-int _isupper(int c)
+ */int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
-}
 }
